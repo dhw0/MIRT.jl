8b7f4675703042f4c4d914d0568b8b35038a3610
Daniel Wan <danielhwann@gmail.com>
2019-10-04 20:47:40 -0400

    Use Julia implmentation of fft functions

--- src/mri/grid_linear.jl
+++ src/mri/grid_linear.jl
@@ -2,6 +2,8 @@ export mri_grid_linear
 
 
 using Interpolations
+using FFTW
+using NFFT
 
 #function [xhat, yhat, xg, yg] = mri_grid_linear(kspace, ydata, N, fov)
     #|function [xhat, yhat, xg, kg] = mri_grid_linear(kspace, ydata, N, fov)
@@ -32,31 +34,34 @@ function mri_grid_linear(kspace, ydata, N, fov)
     if length(fov) .== 1, fov = [fov fov] end
     if length(fov) ~= 2, error "bad fov" end
 
-    kg1 = [-N[1]/2:N[1]/2-1]/fov[1]
-    kg2 = [-N[2]/2:N[2]/2-1]/fov[2]
+    kg = zeros(2)
+    kg[1] = [-N[1]/2:N[1]/2-1]/fov[1]
+    kg[2] = [-N[2]/2:N[2]/2-1]/fov[2]
+
 
     # Fit = interpolate((kg1,kg2),Gridded(Linear()))
     # fitted = [Fit[i,j] for i in kg1, j in kg2]
     # captures ndgrid functionality?
-    k1gg = Float64[i for i in kg1, j in kg2]
-    k2gg = Float64[j for i in kg1, j in kg2]
+    k1gg = i for i in kg[1], j in kg[2]
+    k2gg = j for i in kg[1], j in kg[2]
 
     yhat = interpolate(kspace[:,1], kspace[:,2], ydata, k1gg, k2gg, "linear")
     yhat[isnan(yhat)] = 0
     
+    xg = zeros(2)
     xg[1] = [-N[1]/2:N[1]/2-1]'/N[1] * fov[1]
     xg[2] = [-N[2]/2:N[2]/2-1]'/N[2] * fov[2]
     dk = 1 ./ fov
-    xhat = fftshift(ifft2(fftshift(yhat))) * prod(dk) * prod(N)
-    tmp1 = nufft_sinc[xg[1] / fov[1]]
-    tmp2 = nufft_sinc[xg[2] / fov[2]]
+    xhat = fftshift(ifft(fftshift(yhat))) * prod(dk) * prod(N)
+    tmp1 = nfft[xg[1] / fov[1]]
+    tmp2 = nfft[xg[2] / fov[2]]
     xhat = xhat ./ (tmp1 * tmp2'); # gridding post-correction for linear interp
-    
+end
     
     #
     # test function
     #
-    function mri_grid_linear_test
+function mri_grid_linear_test
     
     fov = 256; # [mm] typical brain FOV
     N0 = 64; # nominal image size()
@@ -66,8 +71,10 @@ function mri_grid_linear(kspace, ydata, N, fov)
     
     Ndisp = 256; # display images like this...
     x1d = [-Ndisp/2:Ndisp/2-1] / Ndisp * fov
-    [x1dd x2dd] = ndgrid(x1d, x1d)
-    
+    x1dd = Float64[i for i in x1d, j in x1d]
+    x2dd = Float64[j for i in x1d, j in x1d]
+
+    #=
     obj = mri_objects["case1"]
     xtrue = obj.image(x1dd, x2dd)
     ytrue = obj.kspace[kspace[:,1], kspace[:,2]]
@@ -81,5 +88,6 @@ function mri_grid_linear(kspace, ydata, N, fov)
     
     im[2, kg(1}, kg[2), abs(yhat), "|y_{grid]|"], cbar
     im[3, xg(1}, xg{2), abs(xhat), "|x| "gridding"", clim], cbar
+    =#
     
 end
